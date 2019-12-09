c4fde0154e5525017ac3c70150fff41764e4ff14
strikeraryu <striker.aryu56@gmail.com>
2019-12-10 00:26:48 +0530

    intail commit

---
+++ crop.py
@@ -0,0 +1,51 @@
+from PIL import Image
+import time
+import os
+import sys
+
+fnd = False
+
+while not fnd:
+    img_path = input("enter image name/path :- ")
+    try:
+        img = Image.open(img_path)
+        fnd = True
+    except Exception as e:
+        print("image not found")
+
+width, height = img.size
+
+len = int(input("enter the side of cropped images :- "))
+
+folder = False
+
+while not folder:
+    title = input("enter the base name :- ")
+    try:
+        os.mkdir(title)
+        folder = True
+    except FileExistsError:
+        print("Error 001 enter new file name :- ")
+
+n = 0
+
+os.system('cls')
+
+for i in range(0, width, len):
+    for j in range(0, height, len):
+        n+=1
+        crp_img = img.crop((i, j, i+len, j+len))
+        path = title + "/" + title + "_" + str(n) +".png"
+        crp_img.save(path)
+        if n%4 == 0:
+            sys.stdout.write('\rloading |')
+        if n%4 == 1:
+            sys.stdout.write('\rloading /')
+        if n%4 == 2:
+            sys.stdout.write('\rloading -')
+        if n%4 == 3:
+            sys.stdout.write('\rloading \\')
+        time.sleep(0.1)
+
+sys.stdout.write('\r!! Done !!')
+time.sleep(5)
---
+++ readme.txt
@@ -0,0 +1,3 @@
+1: enter the path of image from current location of multicrop
+2: enter the len of each cropped images
+3: enter the title for the folder and the base name of each image
